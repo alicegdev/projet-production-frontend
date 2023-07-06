908d5f14df721fbb9a46d7f7bacfea2af1cec525
alicegdev <71830893+alicegdev@users.noreply.github.com>
2023-07-06 18:13:13 +0200

    added proxy and axios to login but query not working (network error)

--- src/components/LoginView.vue
+++ src/components/LoginView.vue
@@ -1,36 +1,44 @@
 <template>
-  <NavbarComp/>
-    <form @submit.prevent="handleSubmit">
-      <h3>Login</h3>
-  
-      <label for="email">Email:</label>
-      <input type="email" name="email" v-model="email" required>
-  
-      <label for="email">Password:</label>
-      <input type="password" name="password" v-model="password" required>
-  
-      <button>Login</button>
-    </form>
-  </template>
-  
-  <script>
-  import { ref } from 'vue'
-  import NavbarComp from './NavbarComp.vue'
+  <NavbarComp />
+  <form @submit.prevent="handleSubmit">
+    <h3>Login</h3>
 
-  export default {
-  name: 'LoginView',
+    <label for="username">Nom d'utilisateur : </label>
+    <input type="username" name="username" v-model="username" required />
+
+    <label for="password">Mot de passe :</label>
+    <input type="password" name="password" v-model="password" required />
+
+    <button>Login</button>
+  </form>
+</template>
+
+<script>
+import { ref } from "vue";
+import axios from "axios";
+import NavbarComp from "./NavbarComp.vue";
+
+export default {
+  name: "LoginView",
   components: {
-    NavbarComp
+    NavbarComp,
   },
-    setup() {
-      const email = ref('')
-      const password = ref('')
-  
-      const handleSubmit = () => {
-        console.log(email.value, password.value)
-      }
-  
-      return { handleSubmit, email, password }
-    }
-  }
-  </script>
+  setup() {
+    const username = ref("username");
+    const password = ref("password");
+
+    const handleSubmit = async (username, password) => {
+      const response = await axios.post(
+        "http://localhost:8000/api/loginAsAdmin",
+        {
+          username: username,
+          password: password,
+        }
+      );
+      console.log(response);
+    };
+
+    return { handleSubmit, username, password };
+  },
+};
+</script>
