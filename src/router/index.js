99a95bbcaa246da18fad6d937b63322e00066a0b
alicegdev <71830893+alicegdev@users.noreply.github.com>
2023-07-13 12:15:46 +0200

    trying to fix routing once again

--- src/router/index.js
+++ src/router/index.js
@@ -1,94 +1,87 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import ChallengeTable from '@/components/ChallengeTable.vue';
-import StudentTable from '@/components/StudentTable.vue';
-import ClassTable from '@/components/ClassTable.vue';
-import HomeView from '@/components/HomeView.vue';
-import LoginView from '@/components/LoginView.vue';
-import ChallengeQuestionTable from '@/components/ChallengeQuestionTable.vue';
-import Admin from '@/components/Admin.vue';
-import ChallengePage from '@/components/ChallengePage.vue';
-import ChallengeQuestionsPage from '@/components/ChallengeQuestionPage.vue';
-import RegisterUser from '@/components/RegisterUser.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import LoginView from "./components/LoginView.vue";
+import ChallengeTable from "./components/ChallengeTable.vue";
+import ChallengeQuestionTable from "./components/ChallengeTable.vue";
+import StudentTable from "./components/StudentTable.vue";
+import ClassTable from "./components/ClassTable.vue";
+import HomeView from "./components/HomeView.vue";
+import Admin from "./components/Admin.vue";
+import ChallengePage from "./components/ChallengePage.vue";
+import ChallengeQuestionsPage from "./components/ChallengeQuestionPage.vue";
+import RegisterUser from "./components/RegisterUser.vue";
+import CryptoJS from "crypto-js";
 
 const routes = [
   {
-    path: '/admin',
-    name: 'AdminTable',
+    path: "/admin",
+    name: "AdminTable",
     component: Admin,
     children: [
       {
-        path: 'challenges',
-        name: 'Challenges',
+        path: "challenges",
+        name: "Challenges",
         component: ChallengeTable,
       },
       {
-        path: 'students',
-        name: 'Students',
+        path: "students",
+        name: "Students",
         component: StudentTable,
       },
       {
-        path: 'classes',
-        name: 'Classes',
+        path: "classes",
+        name: "Classes",
         component: ClassTable,
       },
       {
-        path: 'challengeQuestions',
-        name: 'ChallengeQuestions',
+        path: "challengeQuestions",
+        name: "ChallengeQuestions",
         component: ChallengeQuestionTable,
       },
     ],
   },
-  // Route pour /challenges
   {
-    path: '/challenges',
-    name: 'Challenges',
+    path: "/challenges",
+    name: "Challenges",
     component: ChallengeTable,
   },
-  // Route pour /students
   {
-    path: '/students',
-    name: 'Students',
+    path: "/students",
+    name: "Students",
     component: StudentTable,
   },
-  // Route pour /classes
   {
-    path: '/classes',
-    name: 'Classes',
+    path: "/classes",
+    name: "Classes",
     component: ClassTable,
   },
-  // Route pour /challengequestion
   {
-    path: '/challengequestion',
-    name: 'ChallengeQuestion',
+    path: "/challengequestion",
+    name: "ChallengeQuestion",
     component: ChallengeQuestionTable,
   },
-  // Route pour /challenge/:id
   {
-    path: '/challenge/:id',
-    name: 'ChallengePage',
+    path: "/challenge/:id",
+    name: "ChallengePage",
     component: ChallengePage,
   },
-  // Route pour /challenge/:id/questions
   {
-    path: '/challenge/:id/questions',
-    name: 'ChallengeQuestionsPage',
+    path: "/challenge/:id/questions",
+    name: "ChallengeQuestionsPage",
     component: ChallengeQuestionsPage,
   },
-  // Route pour /register
   {
-    path: '/register',
-    name: 'RegisterUser',
+    path: "/register",
+    name: "RegisterUser",
     component: RegisterUser,
   },
-  // Route pour la page d'accueil
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: HomeView,
     children: [
       {
-        path: '',
-        name: 'Login',
+        path: "",
+        name: "Login",
         component: LoginView,
       },
     ],
@@ -98,6 +91,24 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const jwt = new URLSearchParams(window.location.search).get("jwt");
+  const secretKey = process.env.VUE_APP_SECRET_KEY;
+  const encryptedJWT = CryptoJS.AES.encrypt(jwt, secretKey).toString();
+  const jwtObj = {
+    jwt: encryptedJWT,
+    timestamp: Date.now(),
+  };
+  localStorage.setItem("jwtData", JSON.stringify(jwtObj));
+  console.log("Token set in localStorage");
+
+  if (jwt) {
+    localStorage.setItem("jwt", encryptedJWT);
+  }
+
+  next();
 });
 
 export default router;
